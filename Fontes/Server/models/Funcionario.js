62506872703e9ca55997445593041e1d4fcf0184
Vinicius Zorzanelli <vinizorza@hotmail.com>
2018-07-30 12:00:33 -0300

    Refatoração

---
+++ Fontes/Server/models/Funcionario.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, DataTypes) {
+  var Sequelize = sequelize.Sequelize;
+
+  var funcionario = sequelize.define(
+    "Funcionario",
+    {
+      func_id_funcionario: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      func_ds_email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      func_ds_senha: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      func_ds_nome: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      func_nm_telefone: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      func_nm_cpf: {
+        allowNull: true,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      func_ds_funcao: {
+        allowNull: true,
+        type: Sequelize.STRING
+      }
+    },
+    {
+      timestamps: false,
+      freezeTableName: true
+    }
+  );
+
+
+  return funcionario;
+};
